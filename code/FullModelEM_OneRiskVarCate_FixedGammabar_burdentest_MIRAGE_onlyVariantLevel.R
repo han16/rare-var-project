64e758bfc98bd974d6913dfc74462300707eb885
Shengtong <shengtonghan@gmail.com>
2019-05-21 10:57:14 -0500

    5/21/2019

--- code/FullModelEM_OneRiskVarCate_FixedGammabar_burdentest_MIRAGE_onlyVariantLevel.R
+++ code/FullModelEM_OneRiskVarCate_FixedGammabar_burdentest_MIRAGE_onlyVariantLevel.R
@@ -1,12 +1,13 @@
 # Ui; latent binary variant for gene , Ui~Ber(delta) Ui=1: risk gene Ui=0: non risk gene
-# Zij: latent binary variant for variant, Zij~Ber(pi), Zij=1, variant j of gene i is causal variant, Zij=0 otherwise  
-# m: number of variant 
+# Zij: latent binary variant for variant, Zij~Ber(pi), Zij=1, variant j of gene i is causal variant, Zij=0 otherwise
+# m: number of variant
 # N0: number of controls; N1: number of cases
-# num.gene: number of genes in the sample 
+# num.gene: number of genes in the sample
 ##################################
 rm(list=ls())
-set.seed(123)
+#set.seed(123)
 library(SKAT)
+library(tidyverse)
 
 start.time=date()
 cat("program starts at", date(),"\n\n")
@@ -20,10 +21,10 @@ intergrand=function(aa, indi.var, pheno, bar.gamma, sig)
 BF.gene.inte=function(geno.var, pheno, bar.gamma, sig)
 {
   marglik0.CC <- dbinom(sum(geno.var[pheno==1]), sum(geno.var), N1/(N1+N0))    # Under H0: gamma=1
-  
-  marglik1.CC <- integrate(intergrand, geno.var, pheno, bar.gamma, sig, low=0, upper=100, stop.on.error=F)$value # Under H1: gamma~gamma(gamma.mean*sigma, sigma) 
+
+  marglik1.CC <- integrate(intergrand, geno.var, pheno, bar.gamma, sig, low=0, upper=100, stop.on.error=F)$value # Under H1: gamma~gamma(gamma.mean*sigma, sigma)
   BF.var <- marglik1.CC/marglik0.CC
-  
+
   return(BF.var)
 }
 ##################################
@@ -32,9 +33,9 @@ gene.simu=function(N0, N1, m, alpha0, beta0, alpha, beta, gamma.mean, sigma, pi,
   pheno=c(rep(0,N0), rep(1,N1))  # filtering step
   q <- rbeta(m, alpha0, beta0)
   x1x0=matrix(nrow=(N0+N1), ncol=m)
-  for (i in 1:m)  # generate the data with certain variants full of zeros                
+  for (i in 1:m)  # generate the data with certain variants full of zeros
     x1x0[,i]=rpois((N0+N1), q[i])
-  filter.x1x0=x1x0[,!apply(x1x0==0,2,all)]  # filter variants with 0 counts in both cases and controls since these variants are noninformative 
+  filter.x1x0=x1x0[,!apply(x1x0==0,2,all)]  # filter variants with 0 counts in both cases and controls since these variants are noninformative
   mm=ncol(filter.x1x0)
   ##################
   gamma <- rep(1,mm)
@@ -44,13 +45,13 @@ gene.simu=function(N0, N1, m, alpha0, beta0, alpha, beta, gamma.mean, sigma, pi,
   {
     for (k in 1:num.group)
     {
-      from=split.ratio[k]*mm+1; to=split.ratio[k+1]*mm 
+      from=split.ratio[k]*mm+1; to=split.ratio[k+1]*mm
       Zij[from:to]=rbinom((to-from+1), 1, pi[k])
     }
     qq[Zij==1] <- rbeta(sum(Zij==1), alpha, beta)
     gamma[Zij==1] <- rgamma(sum(Zij==1), gamma.mean*sigma, sigma)
   }
-  
+
   x <- array(0, dim=c(length(pheno), mm))
   for (j in 1:mm)
   {
@@ -60,26 +61,26 @@ gene.simu=function(N0, N1, m, alpha0, beta0, alpha, beta, gamma.mean, sigma, pi,
     x[sample.int(N0,x0),j]=1
     x[N0+sample.int(N1,x1),j]=1
   }
-  
+
   var.index=which(colSums(x1x0!=0)>0) # get the variant index in the original data before filtering
   x=as.matrix(x)
-  return (list(geno=x,pheno=pheno,q=q,gamma=gamma, Zij=Zij, var.index=var.index)) 
-  
+  return (list(geno=x,pheno=pheno,q=q,gamma=gamma, Zij=Zij, var.index=var.index))
+
 }
 ##################################
 num.gene=1
 m=100
-N0=3000; N1=3000
-delta=0
+N0=N1=5000
+delta=1
 alpha0 <- 0.1
 beta0 <- 1000
 alpha <- 0.1
 beta <- 2000
-gamma.mean <- 4
+gamma.mean <- 5
 sigma <- 1
 num.group=1
 split.ratio=c(0, 1)
-pi=0
+pi=0.1
 max.run=100
 all.pi=matrix(nrow=max.run, ncol=1)
 all.teststat=numeric()
@@ -87,14 +88,14 @@ pvalue.fish=numeric()
 all.pvalue=numeric()
 all.conti.table=list()
 odds.ratio=numeric()
-log.lkhd=numeric() # used to check the concavity of likelihood function of beta 
+log.lkhd=numeric() # used to check the concavity of likelihood function of beta
 TP=matrix(nrow=max.run, ncol=3)
 MIRAGE.pvalue=list()
 MIRAGE.BF=list()
 Fisher.pvalue=list()
 SKATO.pvalue=list()
 Gene.Risk.Status=list()
-########################  generate data 
+########################  generate data
 for (run in 1:max.run) {
   actu.no.var=numeric()
   all.data=list()
@@ -111,26 +112,26 @@ for (run in 1:max.run) {
   data=gene.simu(N0, N1, m, alpha0, beta0, alpha, beta, gamma.mean, sigma, pi, Ui, num.group, split.ratio)
 
     # construct  the contigency table for each gene
-    xx=which(data$Zij==1)    # xx; causal variant 
-    yy=data$var.index   # yy: final variant index 
+    xx=which(data$Zij==1)    # xx; causal variant
+    yy=data$var.index   # yy: final variant index
     actu.no.var=length(yy)
-    
-    
-    conti.table=matrix(nrow=2,ncol=2)  # used for fisher exact test 
+
+
+    conti.table=matrix(nrow=2,ncol=2)  # used for fisher exact test
     conti.table[1,1]=sum(data$geno[(data$pheno==1),which(yy%in%data$var.index)]) # var.counts in cases
     conti.table[1,2]=sum(data$pheno==1)*length(yy)-conti.table[1,1]
     conti.table[2,1]=sum(data$geno[(data$pheno==0),which(yy%in%data$var.index)]) # var.counts in controls
     conti.table[2,2]=sum(data$pheno==0)*length(yy)-conti.table[2,1]
-    
+
     conti.matx=conti.table
-    
+
     bb=1
     if (ncol(data$geno)>0) # calculate Bayes factor for variant j
-      for (j in 1:ncol(data$geno)) 
+      for (j in 1:ncol(data$geno))
       {
         orig.var.index=data$var.index[j]
         BF.var[orig.var.index]=BF.gene.inte(data$geno[,j], data$pheno, bar.gamma=gamma.mean, sig=sigma)
-        
+
         bb=bb*((1-beta.k[1])+beta.k[1]*BF.var[orig.var.index])
       }
 
@@ -141,11 +142,11 @@ for (run in 1:max.run) {
   {
     iter=iter+1
 
-    EZj=numeric() # expectation for variant j 
-    
+    EZj=numeric() # expectation for variant j
+
       bb=1
       if (ncol(data$geno)>0)
-        for (j in 1:ncol(data$geno))  
+        for (j in 1:ncol(data$geno))
         {
           orig.var.index=data$var.index[j]
           numer=BF.var[orig.var.index]*beta.k[iter-1]
@@ -164,47 +165,51 @@ for (run in 1:max.run) {
   } # end of iter
   if (iter<max.iter)
     beta.k=beta.k[complete.cases(beta.k)]
-  ################## calculate the likelihood ratio test statistics 
+  ################## calculate the likelihood ratio test statistics
   lkhd=1; total.lkhd=0
     for (j in 1:ncol(data$geno))
     {
       orig.var.index=data$var.index[j]
       lkhd=lkhd*((1-beta.k[iter-1])+beta.k[iter-1]*BF.var[orig.var.index])
-    } # end of j 
+    } # end of j
 
   ##################
   ############# gene level test ###############
-  mirage.test.stat=2*log(lkhd)   # mirage 
-  mirage.pvalue=pchisq(mirage.test.stat, 1, lower.tail=F)  # degrees of freedom depends on the number of unknown parameters 
+  mirage.test.stat=2*log(lkhd)   # mirage
+  mirage.pvalue=pchisq(mirage.test.stat, 1, lower.tail=F)  # degrees of freedom depends on the number of unknown parameters
   fisher.odds.ratio=numeric(); fisher.pvalue=numeric()
   skat.pvalue=numeric()
-  
+
     fish.test=fisher.test(conti.matx)
     fisher.odds.ratio=fish.test$estimate
     fisher.pvalue=fish.test$p.value
     #############  SKAT
-    obj<-SKAT_Null_Model(data$pheno~ 1, out_type="D")  # without covariates 
+    obj<-SKAT_Null_Model(data$pheno~ 1, out_type="D")  # without covariates
     skat.pvalue=SKAT(data$geno, obj, method="SKATO")$p.value
-    
-  
-  
-  
+
+
+
+
   #################
   plot(beta.k, ylim=c(0, 1), main=expression(paste(eta)), xlab="Iteration index", ylab="")
   abline(h=pi, col="red")
-  
+
   all.pi[run]=beta.k[iter-1]
-  
+
   ###################### store statistics for further analysis
   Gene.Risk.Status[[run]]=Ui
   MIRAGE.pvalue[[run]]=mirage.pvalue
   Fisher.pvalue[[run]]=fisher.pvalue
   SKATO.pvalue[[run]]=skat.pvalue
-  
+
 } # end of run
 ######################
 
-summary=tibble(eta_est=all.pi, MIRAGE.pvalue=MIRAGE.pvalue, Fisher.pvalue=Fisher.pvalue, SKATO.pvalue=SKATO.pvalue)
+summary=tibble(eta_est=all.pi[,1], MIRAGE.pvalue=unlist(MIRAGE.pvalue), Fisher.pvalue=unlist(Fisher.pvalue), SKATO.pvalue=unlist(SKATO.pvalue))
+pvalue.cutoff=0.05
+summary %>% filter(MIRAGE.pvalue<pvalue.cutoff) %>% tally()
+summary %>% filter(Fisher.pvalue<pvalue.cutoff) %>% tally()
+summary %>% filter(SKATO.pvalue<pvalue.cutoff) %>% tally()
 
 end.time=date()
 cat("program ends at", date(),"\n\n")
